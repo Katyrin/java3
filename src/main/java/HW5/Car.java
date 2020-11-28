fac25b8632b749b5bcf08c84ba1afe1a21afa7f8
Roman <katyrinroman@mail.ru>
2020-11-28 20:33:05 +0700

    add folder HW5 and files

---
+++ src/main/java/HW5/Car.java
@@ -0,0 +1,54 @@
+package HW5;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Car implements Runnable {
+    private static int CARS_COUNT;
+    static {
+        CARS_COUNT = 0;
+    }
+    private Race race;
+    private int speed;
+    private String name;
+    private CyclicBarrier cb;
+    private AtomicInteger ai;
+
+    public String getName() {
+        return name;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public Car(Race race, int speed, CyclicBarrier cb, AtomicInteger ai) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+        this.cb = cb;
+        this.ai = ai;
+    }
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int)(Math.random() * 800));
+            System.out.println(this.name + " готов");
+            cb.await();
+            if (cb.await() == 0)
+                System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+        }
+
+        int res = ai.incrementAndGet();
+        if (res == 1){
+            System.out.println(this.name + " WIN");
+        }else if (res == 4){
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        }
+    }
+}
